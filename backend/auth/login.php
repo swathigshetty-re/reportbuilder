b45be029e1991db7c492f285554f94bcf383d6bd
swathi <swathigshetty19@gmail.com>
2026-02-12 11:55:35 +0530

    commit

--- backend/auth/login.php
+++ backend/auth/login.php
@@ -1,5 +1,5 @@
 <?php
-require "../config/db.php";   // Adjust path if needed
+require "../backend/config/db.php";   // Adjust path if needed
 
 header("Content-Type: application/json");
 
@@ -7,19 +7,19 @@ header("Content-Type: application/json");
 $data = json_decode(file_get_contents("php://input"));
 
 // Check if data received
-if (!$data || !isset($data->email) || !isset($data->password)) {
+if (!$data || !isset($data->username) || !isset($data->password)) {
     echo json_encode(["status" => "invalid request"]);
     exit;
 }
 
-$email = trim($data->email);
+$username = trim($data->username);
 $password = trim($data->password);
 
 try {
 
     // Prepare statement
-    $stmt = $conn->prepare("SELECT user_id, email, password, role FROM users WHERE email = ?");
-    $stmt->execute([$email]);
+    $stmt = $conn->prepare("SELECT user_id, username, password, role FROM users WHERE username = ?");
+    $stmt->execute([$username]);
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
     // Verify user and password
--- backend/auth/register.php
+++ backend/auth/register.php
@@ -1,5 +1,5 @@
 <?php
-require "../config/db.php";
+require "../backend/config/db.php";
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 include "db.php";
