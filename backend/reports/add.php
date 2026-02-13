855c66efa0a99ceb1c557f5971785c49740fb9db
swathi <swathigshetty19@gmail.com>
2026-02-13 20:05:45 +0530

    initial commit

--- backend/reports/add.php
+++ backend/reports/add.php
@@ -1,21 +1,23 @@
 <?php
+header("Content-Type: application/json");
+
 require "../config/db.php";
 
 $data = json_decode(file_get_contents("php://input"));
 
 $stmt = $conn->prepare(
-    "INSERT INTO reports (report_id, titles, description, status, created_by, created_at, updated_at)
-     VALUES (?, ?, ?, ?, ?, ?, ?)"
+    "INSERT INTO reports 
+    (titles, description, status, created_by, created_at, updated_at)
+    VALUES (?, ?, ?, ?, ?, ?)"
 );
+
 $stmt->execute([
-    $data->report_id,
     $data->title,
     $data->description,
     $data->status,
     $data->created_by,
     $data->created_at,
     $data->updated_at
-
 ]);
 
 echo json_encode(["message" => "Report added"]);
